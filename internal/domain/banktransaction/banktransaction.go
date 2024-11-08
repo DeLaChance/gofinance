60bdeef64384eb4cef9426953ef622ee739ea155
Lucien Valstar <ldjvalstar@gmail.com>
2024-11-08 17:12:25 +0100

    Adds logic for negative and positive amounts

--- internal/domain/banktransaction/banktransaction.go
+++ internal/domain/banktransaction/banktransaction.go
@@ -15,6 +15,14 @@ type BankTransaction struct {
 	remarks string
 }
 
+type BankTransactionType string 
+
+const (
+	UndefinedTransactionType BankTransactionType = ""
+	Credit BankTransactionType = "Credit"
+	Debit BankTransactionType = "Debit"
+)
+
 func (this BankTransaction) ToString() string {
 	return fmt.Sprintf("description=%s, amount=%f, date=%s, remarks=%s, acount=%s", this.description, this.amount, this.date, this.remarks, this.account);
 }
@@ -30,30 +38,50 @@ func FromCsv(csvLines [][]string) []BankTransaction {
 
         log.Printf("Processing line %d - %s", index, csvLine) 
 		transaction := fromCsvSingle(csvLine)
-		transactions = append(transactions, *transaction)
+		if (transaction != nil) {
+			transactions = append(transactions, *transaction)
+		}
     } 
 
 	return transactions
 }
 
 func fromCsvSingle(csvLine []string) *BankTransaction {
-	return &BankTransaction{
-		date: csvLine[0], // TODO: cast to date object 
-		description: csvLine[1], 
-		account: csvLine[2], 
-		amount: parseAmount(csvLine[6]), 
-		remarks: csvLine[8],
+	creditType := transactionTypeFromString(csvLine[5]) // Credit or Debit
+	if (creditType != UndefinedTransactionType) {
+		return &BankTransaction{
+			date: csvLine[0], // TODO: cast to date object 
+			description: csvLine[1], 
+			account: csvLine[2], 
+			amount: parseAmount(csvLine[6], creditType), 
+			remarks: csvLine[8],
+		}
+	} else {
+		return nil
 	}
 }
 
-func parseAmount(stringValue string) float64 {
-	// TODO: cast to negative if deduction
-	stringValue = strings.ReplaceAll(stringValue, ",", ".")
+func parseAmount(amountStringValue string, creditType BankTransactionType) float64 {
+	amountStringValue = strings.ReplaceAll(amountStringValue, ",", ".")
 
-	floatValue, err := strconv.ParseFloat(stringValue, 2)
+	floatValue, err := strconv.ParseFloat(amountStringValue, 2)
 	if err != nil {
-		log.Fatal("Invalid non-number value for: ", stringValue)
+		log.Fatal("Invalid non-number value for: ", amountStringValue)
+	}
+
+	if (creditType == Debit) {
+		floatValue = -1.0 * floatValue;
 	}
 	
 	return floatValue
+}
+
+func transactionTypeFromString(transactionType string) BankTransactionType {
+	if (transactionType == "Credit") {
+		return Credit;
+	} else if (transactionType == "Debit") {
+		return Debit;
+	} else {
+		return UndefinedTransactionType;
+	}
 }
